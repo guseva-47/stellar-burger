8ac0829f86277e8403160f140bad91e8737c0be3
Aleksandra <guseva-47@yandex.ru>
2022-05-18 02:45:07 +0700

    sets in burger ingredients

--- src/burger-ingredients/burger-ingredients.jsx
+++ src/burger-ingredients/burger-ingredients.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import Ingredient from '../ingredient/ingredient'
-import dataSet from "../utils/data"
+import IngredientsSet from "../ingredients-set/ingredients-set";
+import styles from "./burger-ingredients.module.css";
+import dataSet from "../utils/data";
 
 function BurgerIngredients(props) {
-  
-
   return (
-    <section>
-      BurgerIngredients
-      {
-        dataSet.map(data => <Ingredient key={data.id} data={data} />)
-      }
+    <section className={`${styles.burger_ingredients}`}>
+      <IngredientsSet dataSet={dataSet} title="Булки" />
+      <IngredientsSet dataSet={dataSet} title="Булки" />
     </section>
-  )
+  );
 }
 
-BurgerIngredients.propTypes = {}
+BurgerIngredients.propTypes = {};
 
-export default BurgerIngredients
+export default BurgerIngredients;
