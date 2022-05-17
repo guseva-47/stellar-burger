2cc9507b8e4a95bcf493081a752e896cf7a10b29
Aleksandra <guseva-47@yandex.ru>
2022-05-18 01:45:06 +0700

    ingredient looks fine

--- src/ingredient/ingredient.jsx
+++ src/ingredient/ingredient.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Counter,
+  CurrencyIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import styles from "./ingredient.module.css";
 
 // {
 //   _id: "60666c42cc7b410027a1a9b5",
@@ -16,12 +22,26 @@ import PropTypes from 'prop-types'
 //   __v: 0,
 // },
 
-function Ingredient({data}) {
+function Ingredient({ data }) {
   return (
-    <section>{data.name ? data.name : ""}</section>
-  )
+    <article className={`${styles.ingredient}`}>
+      <Counter count={1} size="default" />
+      <div className=" pr-4 pb-1 pl-4">
+        <img src={data.image} alt={data.name} />
+      </div>
+
+      <h3 className={`${styles.price} text text_type_digits-default pb-2`}>
+        <span className="pr-2">{data.price}</span>
+        <CurrencyIcon type="primary" />
+      </h3>
+
+      <h3 className={`${styles.name} text text_type_main-default`}>
+        {data.name}
+      </h3>
+    </article>
+  );
 }
 
-Ingredient.propTypes = {}
+Ingredient.propTypes = {};
 
-export default Ingredient
+export default Ingredient;
