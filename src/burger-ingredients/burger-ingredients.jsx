179d5df812510e4a9da005834189b209ea42a0ca
Aleksandra <guseva-47@yandex.ru>
2022-05-18 19:17:01 +0700

    tabs for ingredients

--- src/burger-ingredients/burger-ingredients.jsx
+++ src/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import IngredientsSet from "../ingredients-set/ingredients-set";
 import styles from "./burger-ingredients.module.css";
@@ -8,27 +9,46 @@ import dataSet from "../utils/data";
 function BurgerIngredients(props) {
   const types = [
     {
-      type: "bun",
+      value: "bun",
       title: "Булки",
     },
     {
-      type: "sauce",
+      value: "sauce",
       title: "Соусы",
     },
     {
-      type: "main",
+      value: "main",
       title: "Начинки",
     },
   ];
 
+  const [current, setCurrent] = useState(types[0].value);
+
   return (
-    <section className={`${styles.burger_ingredients}`}>
-      {types.map(({ type, title }) => {
-        const ingredientsSet = dataSet.filter((data) => data.type === type);
-        return (
-          <IngredientsSet dataSet={ingredientsSet} title={title} key={type} />
-        );
-      })}
+    <section className={`${styles.burger_ingredients} pt-10`}>
+      <h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
+
+      <div className={`${styles.tabs} pb-10`}>
+        {types.map(({ value, title }) => {
+          return (
+            <Tab value={value} active={current === value} onClick={setCurrent}>
+              {title}
+            </Tab>
+          );
+        })}
+      </div>
+      <section>
+        {types.map(({ value, title }) => {
+          const ingredientsSet = dataSet.filter((data) => data.type === value);
+          return (
+            <IngredientsSet
+              dataSet={ingredientsSet}
+              title={title}
+              key={value}
+            />
+          );
+        })}
+      </section>
     </section>
   );
 }
