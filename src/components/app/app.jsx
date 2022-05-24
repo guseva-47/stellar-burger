a8b9854c544d920dd0dfd76aae3ea02bd2642f3a
Aleksandra <guseva-47@yandex.ru>
2022-05-25 00:38:43 +0700

    fix: по правилам eslint + prettier

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,27 +1,27 @@
-import AppHeader from "../app-header/app-header";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import AppHeader from '../app-header/app-header';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
-import styles from "./app.module.css";
-import ingredients from "../../utils/data";
+import styles from './app.module.css';
+import ingredients from '../../utils/data';
 
 function App() {
   const bun = {
-    _id: "60666c42cc7b410027a1a9b1",
-    name: "Краторная булка N-200i",
-    type: "bun",
+    _id: '60666c42cc7b410027a1a9b1',
+    name: 'Краторная булка N-200i',
+    type: 'bun',
     proteins: 80,
     fat: 24,
     carbohydrates: 53,
     calories: 420,
     price: 1255,
-    image: "https://code.s3.yandex.net/react/code/bun-02.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-    image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+    image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
     __v: 0,
   };
 
-  const stuffing = ingredients.filter(elem => elem.type !== "bun");
+  const stuffing = ingredients.filter((elem) => elem.type !== 'bun');
 
   return (
     <div className={styles.app}>
