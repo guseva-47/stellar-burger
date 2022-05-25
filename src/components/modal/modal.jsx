55f03407373e16c217d9679a2f193762af71370a
Aleksandra <guseva-47@yandex.ru>
2022-05-26 01:47:51 +0700

    feat: ingredients-details init

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,14 +3,33 @@ import PropTypes from 'prop-types';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './modal.module.css';
+import IngredientDetails from '../ingredient-details/ingredient-details';
 
 // todo 'детали ингридиента'
 function Modal({ title = 'Детали ингредиента' }) {
+  const ingredient = {
+    _id: '60666c42cc7b410027a1a9b6',
+    name: 'Биокотлета из марсианской Магнолии',
+    type: 'main',
+    proteins: 420,
+    fat: 142,
+    carbohydrates: 242,
+    calories: 4242,
+    price: 424,
+    image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+    __v: 0,
+  };
+
   return (
     <article className={`${styles.modal} p-10 pb-15`}>
       <section className={`${styles.header}`}>
         <h2 className="text text_type_main-large">{title}</h2>
         <CloseIcon type="primary" />
+      </section>
+      <section className="" style={{ display: 'flex', justifyContent: 'center' }}>
+        <IngredientDetails data={ingredient} />
       </section>
     </article>
   );
@@ -21,7 +40,7 @@ Modal.propTypes = {
 };
 
 Modal.defaultProps = {
-  title: '',
+  title: 'Детали ингредиента',
 };
 
 export default Modal;
