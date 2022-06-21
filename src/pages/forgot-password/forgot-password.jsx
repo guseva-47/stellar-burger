491af0d0172c60757252e5d19ef720e916f416e7
Aleksandra <guseva-47@yandex.ru>
2022-06-21 13:33:31 +0700

    style: страницы Забыли пароль, Восстановить пароль

--- src/pages/forgot-password/forgot-password.jsx
+++ src/pages/forgot-password/forgot-password.jsx
@@ -1,5 +1,72 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './forgot-password.module.css';
+
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const [code, setCode] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <form className={`${styles.main} pt-20`}>
+        <h2 className="text text_type_main-medium pb-6">Восстановление пароля</h2>
+
+        {/* Email */}
+        <div className="pb-6">
+          <Input
+            type="email"
+            autocomplete="username"
+            placeholder="Укажите e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            name="email"
+            size="default"
+          />
+        </div>
+
+        {/* New Password */}
+        <div className="pb-6">
+          <PasswordInput
+            autocomplete="new-password"
+            placeholder="Введите новый пароль"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            name="password"
+          />
+        </div>
+
+        {/* Код из письма */}
+        <div className="pb-6">
+          <Input
+            type="text"
+            autocomplete="text"
+            placeholder="Введите код из письма"
+            onChange={(e) => setCode(e.target.value)}
+            value={code}
+            name="code"
+            size="default"
+          />
+        </div>
+
+        <Button type="primary" size="medium">
+          Сохранить
+        </Button>
+      </form>
+
+      <div className={`${styles.main} pt-20`}>
+        <p className="text text_type_main-default">
+          Вспомнили пароль?
+          <Link to="/login" className={styles.link}>
+            {' '}
+            Войти
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default ForgotPassword;
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,5 +1,73 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './register.module.css';
+
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <form className={`${styles.main} pt-20`}>
+        <h2 className="text text_type_main-medium pb-6">Регистрация</h2>
+
+        {/* Имя */}
+        <div className="pb-6">
+          <Input
+            type="text"
+            autocomplete="name"
+            placeholder="Имя"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            name="email"
+            error={false}
+            size="default"
+          />
+        </div>
+
+        {/* Email */}
+        <div className="pb-6">
+          <Input
+            type="email"
+            autocomplete="username"
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            name="email"
+            error={false}
+            size="default"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="pb-6">
+          <PasswordInput
+            autocomplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            name="password"
+          />
+        </div>
+
+        <Button type="primary" size="medium">
+          Зарегистрироваться
+        </Button>
+      </form>
+
+      <div className={`${styles.main} pt-20`}>
+        <p className="text text_type_main-default">
+          Уже зарегистрированы?
+          <Link to="/login" className={styles.link}>
+            {' '}
+            Войти
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default RegisterPage;
