71975318ce97e3379149375236d52b077aac9876
Aleksandra <guseva-47@yandex.ru>
2022-07-14 22:39:20 +0700

    style: анимация иконки галочки "заказ создан"

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,5 +1,5 @@
 import styles from './order-details.module.css';
-import doneIcon from '../../images/done.svg';
+import doneIcon from '../../images/done1repeat.gif';
 
 type Props = {
   number: string;
@@ -10,7 +10,7 @@ function OrderDetails({ number = '' }: Props) {
     <article className={`${styles['order-details']} pt-4`}>
       <h2 className={`${styles.number} text text_type_digits-large pb-8`}>{number}</h2>
       <span className="pb-15 text text_type_main-medium">идентификатор заказа</span>
-      <img className="pb-15" src={doneIcon} alt="ok" />
+      <img className={`${styles.icon} pb-15`} src={doneIcon} alt="ok" />
       <h3 className="pb-2 text text_type_main-default">Ваш заказ начали готовить</h3>
       <span className="pb-15 text text_type_main-default text_color_inactive">
         Дождитесь готовности на орбитальной станции
