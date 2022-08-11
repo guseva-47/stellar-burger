3a4a59cffbec8370922df4f87fc1a1592addbb05
Aleksandra <guseva-47@yandex.ru>
2022-08-11 14:39:43 +0700

    feat: тест для live-feed-reducer

---
+++ src/services/live-feed/live-feed-reducer.test.js
@@ -0,0 +1,47 @@
+import reducer, { setData } from './live-feed.reducer';
+
+const initialState = {
+  orders: [],
+  total: 0,
+  totalToday: 0,
+  status: 'offline',
+};
+
+const orders = {
+  orders: [
+    {
+      ingredients: ['1234'],
+      _id: '1',
+      status: 'done',
+      name: 'order',
+      number: '1',
+      createdAt: '123456',
+      updatedAt: '123456',
+    },
+  ],
+  total: 11,
+  totalToday: 1,
+};
+
+describe('live-feed slice', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should set order, total counts with success status with "setData" action', () => {
+    const payload = { ...orders, success: true };
+    const action = { type: setData.type, payload };
+    const result = reducer(initialState, action);
+
+    expect(result.total).toEqual(11);
+    expect(result.totalToday).toEqual(1);
+    expect(result.orders[0]._id).toEqual('1');
+  });
+
+  it('should set initial state with not success status with "setData" action', () => {
+    const action = { type: setData.type, payload: { ...orders } };
+    const result = reducer(initialState, action);
+
+    expect(result).toEqual(initialState);
+  });
+});
