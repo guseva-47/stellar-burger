0fbe9b5db3a053ab80fee0b8aefd23c10950ed0a
Aleksandra <guseva-47@yandex.ru>
2022-08-15 00:40:41 +0700

    feat: тест, ингредиенты отображаются на главной

--- cypress/e2e/make-order/available-page.cy.ts
+++ cypress/e2e/make-order/available-page.cy.ts
@@ -1,6 +1,35 @@
+import getIngredients from '../../fixtures/constructor.fixture';
+
 describe('Главная страница', () => {
-  it('Страница доступна по адресу localhost:3001', () => {
+  it('Страница доступна по адресу baseUrl', () => {
     cy.visit('/');
-    cy.get('h1').contains("Соберите бургер");
+    cy.contains('Соберите бургер');
+  });
+
+  describe('Ингредиенты отображаются на странице', () => {
+    getIngredients();
+
+    it('Должен появиться ингредиент', () => {
+      cy.wait('@ingredients');
+      cy.contains('Ингредиент 1');
+    });
+  });
+
+  describe('Отображение информации, об отсутствии ингредиентов', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'api/ingredients', { fixture: 'empty-ingredient.json' }).as(
+        'ingredients'
+      );
+      cy.visit('/');
+    });
+
+    it('Должна появиться надпись Загрузка', () => {
+      cy.contains('Загрузка');
+    });
+
+    it('Должна появиться надпись Пусто, когда ингредиенты не загрузились', () => {
+      cy.wait('@ingredients');
+      cy.contains('Пусто');
+    });
   });
 });
